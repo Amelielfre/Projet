ab934d5fa5baf4b405c5336b014b9e88944a0d05
amuset2021 <aurelienmuset@gmail.com>
2022-03-29 15:07:58 +0200

    remember badge

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -14,9 +14,9 @@ class UserController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-       /*  if ($this->getUser()) {
-             return $this->redirectToRoute('app_register');
-         }*/
+         if ($this->getUser()) {
+             return $this->redirectToRoute('app_sortie_creation');
+         }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
