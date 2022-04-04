81b6a1f399e0f7250ee581cf7d9c56e0b8f76f67
OnlyDukeTV <onlyduketv@gmail.com>
2022-04-04 14:49:16 +0200

    Modif photo

--- src/Controller/ModificationProfilController.php
+++ src/Controller/ModificationProfilController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @Route("/profil", name="app_profil_")
@@ -21,9 +22,10 @@ class ModificationProfilController extends AbstractController
     /**
      * @Route("/modifier", name="modifier")
      */
-    public function modifProfil(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $userPasswordHasher): Response
+    public function modifProfil(SluggerInterface $slugger,Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $userPasswordHasher): Response
     {
         $user = $this->getUser();
+        dump($user);
         $errors[] = null;
         $user2 = new User();
         // CREATION FORMULAIRE*****
@@ -55,7 +57,23 @@ class ModificationProfilController extends AbstractController
                     $errors[] = "Les nouveaux mots de passe ne sont pas identiques";
                 }
             }
+            // MODIFIER LA PHOTO
+            $photo = $formModifProfil->get('photo')->getData();
 
+            if ($photo) {
+                $originalFilename = pathinfo($photo->getClientOriginalName(), PATHINFO_FILENAME);
+
+                // this is needed to safely include the file name as part of the URL$safeFilename = $slugger->slug($originalFilename);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$photo->guessExtension();
+
+                // Move the file to the directory where brochures are storedtry {
+                $photo->move(
+                    $this->getParameter('images_directory'),
+                    $newFilename
+                );
+                $user->setUrlPhoto($newFilename);
+            }
             // ENVOI EN BDD
             $em->persist($user);
             $em->flush();
@@ -83,7 +101,7 @@ class ModificationProfilController extends AbstractController
     public function afficherProfil($id, UserRepository $userRepo): Response
     {
         $user = $userRepo->find($id);
-
+        dump($user);
         return $this->render('modification_profil/profil.html.twig', [
             'user' => $user
         ]);
