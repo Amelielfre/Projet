08baf26e8d76fad0069b5c33db8fb748f6634453
-seth <jeremie.velu@outlook.fr>
2022-04-05 08:54:21 +0200

    migration de la table d'archivage

--- migrations/Version20220404140050.php
+++ migrations/Version20220404140050.php
@@ -20,7 +20,7 @@ final class Version20220404140050 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE archives_sorties (id INT AUTO_INCREMENT NOT NULL, nom_lieu VARCHAR(255) NOT NULL, date_debut DATETIME NOT NULL, duree INT DEFAULT NULL, date_fin_inscription DATETIME NOT NULL, nb_inscriptions_max INT NOT NULL, description LONGTEXT DEFAULT NULL, pseudo_organisateur VARCHAR(255) NOT NULL, nom_organisateur VARCHAR(255) NOT NULL, prenom_organisateur VARCHAR(255) NOT NULL, nom_lieu VARCHAR(255) NOT NULL, cp_ville VARCHAR(5) NOT NULL, nom_ville VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE archives_sorties (id INT AUTO_INCREMENT NOT NULL, nom_sortie VARCHAR(255) NOT NULL, date_debut DATETIME NOT NULL, duree INT DEFAULT NULL, date_fin_inscription DATETIME NOT NULL, nb_inscriptions_max INT NOT NULL, description LONGTEXT DEFAULT NULL, pseudo_organisateur VARCHAR(255) NOT NULL, nom_organisateur VARCHAR(255) NOT NULL, prenom_organisateur VARCHAR(255) NOT NULL, nom_lieu VARCHAR(255) NOT NULL, cp_ville VARCHAR(5) NOT NULL, nom_ville VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
